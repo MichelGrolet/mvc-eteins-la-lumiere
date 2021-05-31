21668500b0393cf5eddea0c4a44877318f93dc6f
grolet4u <grolet4u@etu.univ-lorraine.fr>
2021-05-31 11:10:25 +0200

    vues et principale

--- src/Principale.java
+++ src/Principale.java
@@ -1,3 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.BorderLayout;
+
 public class Principale {
-	
+	VueBoutons vueBoutons = new VueBoutons();
+	VueTerrain vueTerrain = new VueTerrain();
+
+	JFrame f = new JFrame();
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	f.setLayout(new BorderLayout());
 }
--- src/VueTerrain.java
+++ src/VueTerrain.java
@@ -1,12 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
-import javax.swing.JPanel;
 
 public class VueTerrain extends JPanel implements Observer {
+
+	public VueTerrain() {
+
+	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		
 	}
+
+	@Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+    }
 	
 }
