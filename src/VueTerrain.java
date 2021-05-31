79f68a9665783e32225237068f776cfe1cbfb612
grolet4u <grolet4u@etu.univ-lorraine.fr>
2021-05-31 10:52:57 +0200

    creation classes

---
+++ src/VueTerrain.java
@@ -0,0 +1,12 @@
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JPanel;
+
+public class VueTerrain extends JPanel implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		
+	}
+	
+}
