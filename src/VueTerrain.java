12c90c6911e1b74f6bcbd44d5ff6a495232f24da
Gabin Chassard <gabin.chassard2@etu.univ-lorraine.fr>
2021-06-01 10:56:24 +0200

    Vue terrain

--- src/VueTerrain.java
+++ src/VueTerrain.java
@@ -4,26 +4,57 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class VueTerrain extends JPanel implements Observer {
-
-
+	/**
+    * Terrain contenant les lampes
+    */
 	private Terrain t;
 
+	/**
+	* Constructeur de la VueTerrain
+	* @param t un terrain
+	*/
 	public VueTerrain(Terrain t) {
-		this.t = t;
-		this.setPreferredSize(new Dimension(500, 500));
-		this.setLayout(new GridLayout(5, 5));
-
+		this.t=t;
+		this.setPreferredSize(new Dimension(500,500));
 	}
 
+	/**
+	* Met a jour graphiquement les changements du modele
+	*/
 	@Override
 	public void update(Observable o, Object arg) {
-		
+		repaint();
 	}
 
+	/**
+	* Permet de dessiner le modele
+	*/
 	@Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
         g.setColor(Color.BLACK);
+        for(int i=1;i<6;i++){
+			for(int j=0;j<5;j++){
+				g.drawRect(this.t.getLampes().get(i*j).getX(),this.t.getLampes().get(i*j).getY(),
+				this.t.getLampes().get(i*j).getX()+Lampe.TAILLE,this.t.getLampes().get(i*j).getY()+Lampe.TAILLE);
+			} 
+        }
+
+        for(int i=1;i<6;i++){
+			for(int j=0;j<5;j++){
+				if(this.t.getLampes().get(i*j).getAllume()){
+					g.setColor(Color.GREEN);
+					g.fillRect(this.t.getLampes().get(i*j).getX()+1,this.t.getLampes().get(i*j).getY()+1,
+					(this.t.getLampes().get(i*j).getX()+Lampe.TAILLE)-1,(this.t.getLampes().get(i*j).getY()+Lampe.TAILLE)-1);
+				}
+				else{
+					g.setColor(Color.GRAY);
+					g.fillRect(this.t.getLampes().get(i*j).getX()+1,this.t.getLampes().get(i*j).getY()+1,
+					(this.t.getLampes().get(i*j).getX()+Lampe.TAILLE)-1,(this.t.getLampes().get(i*j).getY()+Lampe.TAILLE)-1);
+				}
+			} 
+        }
     }
 	
 }
