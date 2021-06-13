acbd0e5945176ca50f462e2e171e49f87da5e676
Gabin Chassard <gabin.chassard2@etu.univ-lorraine.fr>
2021-06-13 11:57:56 +0200

    je suis un bg

--- src/VueTerrain.java
+++ src/VueTerrain.java
@@ -39,17 +39,10 @@ public class VueTerrain extends JPanel implements Observer{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-		if(!this.t.getGagne() || this.t.modeValeur("configuration") || this.t.modeValeur("inactif")){
+		//if(!this.t.getGagne() || this.t.modeValeur("configuration") || this.t.modeValeur("inactif")){
 			//On dessine un carre pour chaque lampes,
-			//Gris si eteinte, vert si allumee
 			
 			for(int i=0;i<25;i++){
-				if(this.t.getLampes()[i].getAllume()){
-					g.setColor(Color.decode("#fff2c7"));
-				}
-				else{
-					g.setColor(Color.decode("#705600"));
-				}
 				int[] x={this.t.getLampes()[i].getX()+decalage,this.t.getLampes()[i].getX()+Lampe.TAILLE-decalage,
 						this.t.getLampes()[i].getX()+Lampe.TAILLE,this.t.getLampes()[i].getX()+Lampe.TAILLE,
 						this.t.getLampes()[i].getX()+Lampe.TAILLE-decalage,this.t.getLampes()[i].getX()+decalage,
@@ -58,19 +51,52 @@ public class VueTerrain extends JPanel implements Observer{
 						this.t.getLampes()[i].getY()+decalage,this.t.getLampes()[i].getY()-decalage+Lampe.TAILLE,
 						this.t.getLampes()[i].getY()+Lampe.TAILLE,this.t.getLampes()[i].getY()+Lampe.TAILLE,
 						this.t.getLampes()[i].getY()-decalage+Lampe.TAILLE,this.t.getLampes()[i].getY()+decalage};
-				g.fillPolygon(x,y,8);
-				g.fillOval(this.t.getLampes()[i].getX(),this.t.getLampes()[i].getY(),decalage*2,decalage*2);
-				g.fillOval(this.t.getLampes()[i].getX()+Lampe.TAILLE-decalage*2,this.t.getLampes()[i].getY(),decalage*2,decalage*2);
-				g.fillOval(this.t.getLampes()[i].getX()+Lampe.TAILLE-decalage*2,this.t.getLampes()[i].getY()+Lampe.TAILLE-decalage*2,decalage*2,decalage*2);
-				g.fillOval(this.t.getLampes()[i].getX(),this.t.getLampes()[i].getY()+Lampe.TAILLE-decalage*2,decalage*2,decalage*2);	
+
+				if(this.t.getLampes()[i].getAllume()){
+					g.setColor(new Color(0,250,0));
+					g.fillPolygon(x,y,8);
+					g.fillOval(this.t.getLampes()[i].getX(),this.t.getLampes()[i].getY(),decalage*2,decalage*2);
+					g.fillOval(this.t.getLampes()[i].getX()+Lampe.TAILLE-decalage*2,this.t.getLampes()[i].getY(),decalage*2,decalage*2);
+					g.fillOval(this.t.getLampes()[i].getX()+Lampe.TAILLE-decalage*2,this.t.getLampes()[i].getY()+Lampe.TAILLE-decalage*2,decalage*2,decalage*2);
+					g.fillOval(this.t.getLampes()[i].getX(),this.t.getLampes()[i].getY()+Lampe.TAILLE-decalage*2,decalage*2,decalage*2);
+				
+				for(int j=Lampe.TAILLE/2-decalage/2;j>0;j--){
+					int v=125;
+					Color coul=new Color(0,v+3*j,0);
+					g.setColor(coul);
+					g.fillRect((this.t.getLampes()[i].getX()+Lampe.TAILLE)-Lampe.TAILLE/2-j,(this.t.getLampes()[i].getY()+Lampe.TAILLE)-Lampe.TAILLE/2-j,j*2,j*2);
+				}
+				}
+				else{
+					g.setColor(new Color(0,130,0));
+					g.fillPolygon(x,y,8);
+					g.fillOval(this.t.getLampes()[i].getX(),this.t.getLampes()[i].getY(),decalage*2,decalage*2);
+					g.fillOval(this.t.getLampes()[i].getX()+Lampe.TAILLE-decalage*2,this.t.getLampes()[i].getY(),decalage*2,decalage*2);
+					g.fillOval(this.t.getLampes()[i].getX()+Lampe.TAILLE-decalage*2,this.t.getLampes()[i].getY()+Lampe.TAILLE-decalage*2,decalage*2,decalage*2);
+					g.fillOval(this.t.getLampes()[i].getX(),this.t.getLampes()[i].getY()+Lampe.TAILLE-decalage*2,decalage*2,decalage*2);
+				
+					for(int j=Lampe.TAILLE/2-decalage/2;j>0;j--){
+						int v=5;
+						Color coul=new Color(0,v+3*j,0);
+						g.setColor(coul);
+						g.fillRect((this.t.getLampes()[i].getX()+Lampe.TAILLE)-Lampe.TAILLE/2-j,(this.t.getLampes()[i].getY()+Lampe.TAILLE)-Lampe.TAILLE/2-j,j*2,j*2);
+					}
+				}	
 			}
-		}
-		else{
-			g.setColor(Color.BLACK);
+		
+		if(this.t.getGagne()){
+			g.setColor(Color.YELLOW);
 			g.setFont(new Font("Verdana", Font.BOLD, 28));
-			String txt = "Temps : "+(int)(t.getCompteur()/1000)+" seconde(s) !";
+			String txt;
+			if((int)(t.getCompteur()/1000)<2){
+				txt = "Temps : "+(int)(t.getCompteur()/1000)+" seconde !";
+			}
+			else{
+				txt = "Temps : "+(int)(t.getCompteur()/1000)+" secondes !";
+			}
 			g.drawString("Vous avez gagne ! ",100,200);
 			g.drawString(txt,65,280);
+			g.drawString("Nombre de clic : "+this.t.getNbClic()+" !",65,320);
 			t.setGagne();
 			t.setMode("inactif");
 		}
